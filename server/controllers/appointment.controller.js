c4893377b4cfa9d5ec279e5ce4d2736e4dd9e552
zeinebkhemida <zeineb.khemida11@gmail.com>
2023-10-17 18:46:50 +0100

    done

---
+++ server/controllers/appointment.controller.js
@@ -0,0 +1,33 @@
+
+
+const prisma = require('../prisma');
+
+module.exports = {
+    create: async (req, res) => {
+        try {
+            const response = await prisma.appointments.create({
+                data: req.body,
+            });
+            res.json(response);
+        } catch (error) {
+            res.status(500).json(error); 
+        }
+    },
+    update: async (req, res) => {
+        try {
+            const response = await prisma.appointments.update({
+                where: { id: req.params.id },
+                data: req.body,
+            });
+            res.status(201).json(response); 
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+};
+
+
+
+
+
+
