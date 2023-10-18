a8dbe8cf8d585923f3e21e32318b95e29e951088
yjandoubi95 <58791201+yjandoubi95@users.noreply.github.com>
2023-10-18 10:19:17 +0100

    Schedule

---
+++ client/src/app/doctorProfile/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+import Calendar from 'react-calendar';
+const CalendarComponent = () => {
+  return (
+      <Calendar className="schedule-calendar"/>
+  )
+}
+
+export default CalendarComponent
