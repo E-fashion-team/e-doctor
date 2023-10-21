46801596e1b6b48601541b8b371cb3c171bfaa4e
LouhichiMedSelim <lmedselim@gmail.com>
2023-10-21 10:24:54 +0100

    eee

---
+++ client/src/app/patcalldoc/pagee.tsx
@@ -0,0 +1,11 @@
+// "use client"
+// import React from "react";
+// import  callDoc  from "./call_doc/page"
+// const patcalldoc=()=>{
+// return (
+//     <div>
+//       <callDoc/>  
+//     </div>
+// )
+// }
+// export default patcalldoc
